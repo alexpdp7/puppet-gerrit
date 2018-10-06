b74763ca5a275adf4d15c4cd1f2b424e25e37ec9
alex <alex@pdp7.net>
2018-10-06 12:01:52 +0200

    Initialize gerrit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class gerrit {
-  package {'java-1.8.0-openjdk-headless':}
+  package {['java-1.8.0-openjdk-headless', 'git']:}
 
   user {'gerrit':
     ensure => present,
@@ -8,5 +8,13 @@ class gerrit {
   ->
   file {'/home/gerrit/gerrit.war':
     source => 'https://gerrit-releases.storage.googleapis.com/gerrit-2.15.4.war',
+  }
+
+  exec {'init gerrit':
+    command => '/usr/bin/java -jar /home/gerrit/gerrit.war init -d /home/gerrit/site/',
+    user => 'gerrit',
+    require => [Package['java-1.8.0-openjdk-headless'],
+                File['/home/gerrit/gerrit.war']],
+    creates => '/home/gerrit/site',
   }
 }
